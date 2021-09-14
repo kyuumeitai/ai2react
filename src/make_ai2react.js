c325bc3559a9530acdbda17be0ef4a7b1b2315af
Álex Acuña Viera @ Nataku WSL2 <kyumeitai@gmail.com>
2021-09-14 02:01:33 -0300

    (chore) test

--- src/make_ai2react.js
+++ src/make_ai2react.js
@@ -1,30 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import camelCase from 'camelcase'
-import { useResizing } from './hooks'
+import React from "react";
+import styled from "styled-components";
+import camelCase from "camelcase";
 
 export function makeAi2React(opts = {}) {
-  const {
-    html,
-    styles,
-    containerId,
-    namespace = 'g-',
-    name = containerId,
-  } = opts
+  const { html, styles, containerId, name = containerId } = opts;
 
   const Wrapper = styled.div`
     ${styles}
-  `
+  `;
 
   function Component(props) {
-    const ref = useResizing({ containerId, namespace })
-    const __html = (typeof html === 'function') ? html(props) : html
-    return (
-      <Wrapper ref={ref} dangerouslySetInnerHTML={{ __html }} />
-    )
+    const __html = typeof html === "function" ? html(props) : html;
+    return <Wrapper dangerouslySetInnerHTML={{ __html }} />;
   }
 
-  Component.displayName = camelCase(name, { pascalCase: true })
+  Component.displayName = camelCase(name, { pascalCase: true });
 
-  return Component
+  return Component;
 }
